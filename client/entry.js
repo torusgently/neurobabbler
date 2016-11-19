5a914189b4681aa970c03bc19207ed5c929a4d3a
Heiko Donat <donat@fzi.de>
2016-11-19 17:18:22 +0100

    push the messed up sh**

--- client/entry.js
+++ client/entry.js
@@ -1,1 +1,2 @@
-require('./modules/app.module.ts');
+require('shim')
+require('./app.module.ts');
--- server/app.py
+++ server/app.py
@@ -1,15 +1,11 @@
 from flask import Flask, jsonify, current_app, render_template
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def index():  
+def index():
     return render_template('index.html')
 
-
-
-
-     
 
 @app.route('/data')
 def names():
