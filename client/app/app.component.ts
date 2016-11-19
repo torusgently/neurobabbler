c7b23b07013184088a3473d8519ac4d3547a9252
Heiko Donat <donat@fzi.de>
2016-11-19 20:52:39 +0100

    back to old stuff

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -3,26 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   styles: [`
-    
   `],
   template: `
     <div class="jumbotron text-center">
       <h1>The App Lives!</h1>
       <p>{{ message }}</p>
-      <chart [options]="options"></chart>
     </div>
   `
 })
 export class AppComponent {
     message = 'And this is a Chart!';
 
-    constructor() {
-        this.options = {
-            title : { text : 'simple chart' },
-            series: [{
-                data: [29.9, 71.5, 106.4, 129.2],
-            }]
-        };
-    }
-    options: Object;
 }
