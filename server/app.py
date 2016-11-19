c7faf7a0c020013b8d0a2314873b86f3eb39e9fb
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-11-19 15:43:23 +0100

    Commit

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, current_app
+from flask import Flask, jsonify, current_app, render_template
 
 app = Flask(__name__, static_url_path='/')
 
 @app.route('/')
 def index():  
-    return current_app.send_static_file('index.html')
+    return render_template('index.html')
+
+
+
+
      
 
 @app.route('/data')
