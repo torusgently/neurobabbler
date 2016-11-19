aa29033551b7470a55a79eda9581b0b698afb6b1
Heiko Donat <donat@fzi.de>
2016-11-19 21:06:10 +0100

    adds zingchart

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ZingChart } from './zingchart.component';
+import { Chart } from './chart.model';
 @Component({
   selector: 'my-app',
   styles: [`
@@ -8,10 +10,26 @@ import { Component } from '@angular/core';
     <div class="jumbotron text-center">
       <h1>The App Lives!</h1>
       <p>{{ message }}</p>
+      <zingchart *ngFor="let chart of charts" [chart]="chart"></zingchart>
     </div>
-  `
+`,
+    directives: [ZingChart]
 })
 export class AppComponent {
     message = 'And this is a Chart!';
-
+    charts: Chart[];
+    constructor () {
+    this.charts = [
+      {
+        id: 'chart-1',
+        data: {
+          type: 'bar',
+          series: [{
+            values: [2, 3, 4, 5, 3, 3, 2]
+          }]
+        },
+        height: 300
+      }
+    ]
+  }
 }
---
+++ client/app/chart.model.ts
@@ -0,0 +1,13 @@
+export class Chart {
+  id: String;
+  data: Object;
+  height: any;
+  width: any;
+  
+  constructor (config: Object) {
+    this.id = config.id;
+    this.data = config.data;
+    this.height = config.height || 400;
+    this.width = config.width || '100%';
+  }
+}
