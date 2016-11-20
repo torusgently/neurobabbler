2f21a509d0d77fac8dda4e6c144f5a38b3093c44
Heiko Donat <donat@fzi.de>
2016-11-20 10:46:12 +0100

    adds load state but with one bug

--- client/src/app/network.component.ts
+++ client/src/app/network.component.ts
@@ -34,12 +34,31 @@ export class Network implements OnChanges{
   }
 
   ngOnChanges(changes: SimpleChanges) {
-
+      console.log(changes);
       var prev = changes["state"].previousValue;
       var curr = changes["state"].currentValue;
-
+      if(typeof curr === 'undefined') {
+          console.log("No Current State");
+      } else{
+          console.log(curr);
       //Logik...
+          for(let el in curr) {
+              console.log(el)
+          if(typeof this.layers[el.id] === 'undefined') {
+              var layer = new Layer({'id': el.id, 'weights': el.weights, 'bias': el.bias});
+              // layer.id =  curr[i].id;
+              // layer.weights =  curr[i].weights;
+              // layer.bias =  curr[i].bias;
+              this.layers.push(layer);
+          }
+          else {
+              this.layers[el.id].id = el.id;
+              this.layers[el.id].weights = el.weights;
+              this.layers[el.id].bias = el.bias;
+          }
 
+      }
+      }
   }
 
 
